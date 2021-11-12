e55603687b363d0d6ff7a6ccc1c30dc241b4a534
Jangerud <85johjan@gafe.molndal.se>
2021-11-12 11:35:03 +0100

    New code.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EnvironmentT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!HASIUDHAUSA");
         }
     }
 }
